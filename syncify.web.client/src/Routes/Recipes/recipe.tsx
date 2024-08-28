076afc780502dfbdee2aa88e960a4dd9d89782b6
Edward Christian <edward.christian@selu.edu>
2024-08-27 22:05:59 -0500

    added modals, some more routing stuff, overall styling of recipe page
    added react modal - added images to recipes - created and modified modal for recipe page - modified css so it will take up whole page

--- syncify.web.client/src/Routes/Recipes/recipe.tsx
+++ syncify.web.client/src/Routes/Recipes/recipe.tsx
@@ -1,11 +1,105 @@
-
-import React from 'react';
+// src/Recipes.tsx
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import Modal from 'react-modal';
+import './recipe.css';
+import Melon from '../../Images/Melon.jpg';
+import Pear from '../../Images/Pear.jpg';
+import RandomFood from '../../Images/Random Food.jpg';
+
+Modal.setAppElement('#root'); // Important for accessibility
+
+const recipeData = [
+    {
+        id: 1,
+        title: 'Melon Drama Delight',
+        image: Melon,
+        bio: 'This is just a melon, however long it takes you to go to grocey store.',
+        timeToPrepare: '15 minutes',
+        author: 'Chef Melon',
+        moreInfoLink: '/recipes',
+    },
+    {
+        id: 2,
+        title: 'Pair-a-dise Pearfection',
+        image: Pear,
+        bio: 'This is just a pear.',
+        timeToPrepare: 'How long it takes you to go to grocery store',
+        author: 'Chef Pear',
+        moreInfoLink: '/recipes',
+    },
+    {
+        id: 3,
+        title: 'Chaos Cuisine Medley',
+        image: RandomFood,
+        bio: 'AHHHHHHHHHHHHHHHHHHHHHH!',
+        timeToPrepare: '40 decades',
+        author: 'Chef Chaos',
+        moreInfoLink: '/recipes',
+    },
+    
+];
 
 export default function Recipes() {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [selectedRecipe, setSelectedRecipe] = useState<any>(null);
+
+    const openModal = (recipe: any) => {
+        setSelectedRecipe(recipe);
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+        setSelectedRecipe(null);
+    };
+
     return (
-        <div>
-            This is where Recipes will go!
+        <div className="container">
+            <header className="header">whatever header we decide to go with</header>
+            <div className="sidebar">
+                <div className="menu-item">Your Recipes</div>
+                <div className="menu-item">Friends Recipes</div>
+                <div className="menu-item">Invite / Join More Friends</div>
+            </div>
+            <div className="content">
+                {recipeData.map((recipe) => (
+                    <div
+                        key={recipe.id}
+                        className="section"
+                        onClick={() => openModal(recipe)}
+                    >
+                        <img src={recipe.image} alt={recipe.title} className="recipe-image" />
+                        <p className="recipe-title">{recipe.title}</p>
+                    </div>
+                ))}
+            </div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                className="modal"
+                overlayClassName="modal-overlay"
+            >
+                {selectedRecipe && (
+                    <div className="modal-content">
+                        <h2>{selectedRecipe.title}</h2>
+                        <img src={selectedRecipe.image} alt={selectedRecipe.title} />
+                        <p>{selectedRecipe.bio}</p>
+                        {/* Time to Prepare */}
+                        <p><strong>Time to Prepare:</strong> {selectedRecipe.timeToPrepare}</p>
+                        {/* Author of the recipe */}
+                        <p><strong>Author:</strong> {selectedRecipe.author}</p>
+                        {/* More Info button with hyperlink */}
+                        <button
+                            onClick={() => window.open(selectedRecipe.moreInfoLink, '_blank')}
+                            className="more-info-button"
+                        >
+                            More Info
+                        </button>
+                        <button onClick={closeModal}>Close</button>
+                    </div>
+                )}
+            </Modal>
             <Outlet />
         </div>
     );
