593a0bbd6a629bd187b85f690b4d5c921535eeb8
sbancevic <98131065+sbancevic@users.noreply.github.com>
2024-08-27 20:40:05 -0500

    Update User.cs

--- Syncify.Web.Server/Features/Users/User.cs
+++ Syncify.Web.Server/Features/Users/User.cs
@@ -8,4 +8,5 @@ public class User : IdentityUser<int>
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
     public virtual ICollection<UserRole> Roles { get; set; } = new List<UserRole>();
+    public virtual ICollection<Recipe> Recipes { get; set; } = new List<Recipe>();
 }
