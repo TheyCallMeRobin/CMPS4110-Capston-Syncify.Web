44e4fa20d514ade9936136788cd47746b3907130
Samuel Echols <samuel.echols@selu.edu>
2024-08-28 21:18:17 -0500

    feat: created data seeding for users and roles

--- Syncify.Web.Server/Program.cs
+++ Syncify.Web.Server/Program.cs
@@ -1,8 +1,11 @@
 using System.Reflection;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Syncify.Web.Server.Data;
+using Syncify.Web.Server.Features.Authorization;
+using Syncify.Web.Server.Features.Users;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +16,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMvc();
-builder.Services.AddDbContext<DataContext>(opts => opts.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(opts => opts.UseSqlServer(connectionString));
+builder.Services.AddIdentity<User, Role>().AddEntityFrameworkStores<DataContext>();
 
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddFluentValidationAutoValidation();
@@ -24,9 +29,18 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
+    
     var dataContext = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<User>>();
+    var roleManager = services.GetRequiredService<RoleManager<Role>>();
     
     await dataContext.Database.MigrateAsync();
+
+    if (app.Environment.IsDevelopment())
+    {
+        var dataSeeder = new DataSeeder(dataContext, userManager, roleManager);
+        await dataSeeder.SeedData();
+    }
 }
 
 app.UseDefaultFiles();
