1684dd78b8148d44e969175df1df78d34ff4bb15
Samuel Echols <samuel.echols@selu.edu>
2024-09-16 15:04:10 -0500

    feat: created migrations

---
+++ Syncify.Web.Server/Migrations/20240916194619_Add_CalendarEvents.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Syncify.Web.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_CalendarEvents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CalendarEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CalendarId = table.Column<int>(type: "int", nullable: false),
+                    CreatedByUserId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(2056)", maxLength: 2056, nullable: false),
+                    DisplayColor = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    StartTime = table.Column<TimeOnly>(type: "time", nullable: true),
+                    EndTime = table.Column<TimeOnly>(type: "time", nullable: true),
+                    CalendarEventType = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    RecurrenceType = table.Column<int>(type: "int", nullable: true),
+                    RecurrenceEndDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    RecurrenceWeekDays = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: -1, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalendarEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CalendarEvents_AspNetUsers_CreatedByUserId",
+                        column: x => x.CreatedByUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_CalendarEvents_Calendars_CalendarId",
+                        column: x => x.CalendarId,
+                        principalTable: "Calendars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CalendarEvents_CalendarId",
+                table: "CalendarEvents",
+                column: "CalendarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CalendarEvents_CreatedByUserId",
+                table: "CalendarEvents",
+                column: "CreatedByUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalendarEvents");
+        }
+    }
+}
