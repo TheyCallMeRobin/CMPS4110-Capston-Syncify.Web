7ec355da8dc5e0bdb61119e53b9fb6ce06329c82
Samuel Echols <samuel.echols@selu.edu>
2024-09-18 12:04:54 -0500

    fix: Fixed nullability of Description

--- Syncify.Web.Server/Features/ShoppingLists/ShoppingList.cs
+++ Syncify.Web.Server/Features/ShoppingLists/ShoppingList.cs
@@ -21,12 +21,9 @@ public class ShoppingListEntityConfiguration : IEntityTypeConfiguration<Shopping
     {
         builder.ToTable("ShoppingLists");
 
+        builder.Property(x => x.Description).HasMaxLength(128);
         builder.Property(x => x.Name).HasMaxLength(128).IsRequired();
-        builder.Property(x => x.Description).HasMaxLength(128).IsRequired();
-
-        builder.Property(x => x.Checked).IsRequired();
-        builder.Property(x => x.Completed).IsRequired();
-
+        
         builder.HasOne(x => x.User)
             .WithMany(x => x.ShoppingLists)
             .HasForeignKey(x => x.UserId);
