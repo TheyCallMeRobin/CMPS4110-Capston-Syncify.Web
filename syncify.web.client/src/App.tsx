d955f975df392878fbb55c3a97c7193be0e1fb18
Samuel Echols <samuel.echols@selu.edu>
2024-09-18 12:09:39 -0500

    fixed initialization file structure - should not change

--- syncify.web.client/src/App.tsx
+++ syncify.web.client/src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { MyAppContextProvider } from './Context/MyAppContext';
 import './App.css';
+import { Router } from './routes.tsx';
 
 export const App: React.FC = () => {
   return (
       <>
         <MyAppContextProvider>
-          <div className="App">
-            <header className="App-header">
-              <h1>Welcome to Syncify</h1>
-            </header>
-          </div>
+          <Router />
         </MyAppContextProvider>
       </>
   );
