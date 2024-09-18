9db1edc8640a3f1116ef61373e8db754de0176a3
Robin White <104403739+TheyCallMeRobin@users.noreply.github.com>
2024-09-17 21:05:25 -0500

    Update syncify.web.client/src/App.tsx
    Co-authored-by: Samuel Echols <samuel.echols@selu.edu>

--- syncify.web.client/src/App.tsx
+++ syncify.web.client/src/App.tsx
@@ -18,30 +18,3 @@ const App: React.FC = () => {
 
 export default App;
 
-
-/*
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {MainPage} from './MainPage.tsx';
-
-import './api/generated/config';
-
-
-import './App.css';
-import ShoppingLists from './Routes/ShoppingLists/shoppinglists.tsx';
-
-const App: React.FC = () => {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/shopping-list" element={<ShoppingLists /> } />
-                </Routes>
-            </div>
-        </Router>
-    );
-};
-
-export default App;
-*/
