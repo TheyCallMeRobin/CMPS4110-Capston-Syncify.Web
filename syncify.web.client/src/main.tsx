b015ec4ee2062b8860e21ebc2992455703a3cb8d
Robin White <104403739+TheyCallMeRobin@users.noreply.github.com>
2024-09-20 22:15:42 -0500

    Update main.tsx

--- syncify.web.client/src/main.tsx
+++ syncify.web.client/src/main.tsx
@@ -49,7 +49,7 @@ const router = createBrowserRouter([
                 element: <Reminders />,
             },
             {
-                path: "/shopping-list",
+                path: "/shoppinglists",
                 element: <ShoppingLists />,
             },
         ],
