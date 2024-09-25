697a16405287eecf72ecee6fb87224e2a46470ae
Samuel Echols <samuel.echols@selu.edu>
2024-09-25 14:00:15 -0500

    feat: created a special character validation extension

---
+++ Syncify.Web.Server/Extensions/ValidationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Syncify.Web.Server.Extensions;
+
+public static class ValidationExtensions
+{
+    private static readonly char[] SpecialCharacters =
+    [
+        '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+        '-', '_', '=', '+', '[', ']', '{', '}', '\\', '|',
+        ';', ':', '\'', '\"', ',', '<', '.', '>', '/', '?',
+        '`', '~'
+    ];
+
+    public static IRuleBuilderOptions<T, string> HasSpecialCharacters<T>(this IRuleBuilder<T, string> ruleBuilder,
+        int count = 1)
+    {
+        return ruleBuilder
+            .Must(str => str.Count(c => SpecialCharacters.Contains(c)) >= count)
+            .WithMessage("{PropertName} must have at least " + count + " special characters.");
+    }
+}
