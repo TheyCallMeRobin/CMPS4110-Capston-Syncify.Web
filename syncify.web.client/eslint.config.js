bfde767b19f02a375066755dc6832c486edc849e
Samuel Echols <samuel.echols@selu.edu>
2024-09-25 13:28:51 -0500

    feat: fixed prettier config and missing dependency

--- syncify.web.client/eslint.config.js
+++ syncify.web.client/eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist', 'src/api/'] },
@@ -34,7 +34,7 @@ export default tseslint.config(
       "@typescript-eslint/ban-ts-comment": 'off',
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": 'warn',
-      "@typescript-esling/no-explicit-any": 'off',
+      "@typescript-eslint/no-explicit-any": 'off',
       "no-restricted-syntax": [
         "error",
         {
