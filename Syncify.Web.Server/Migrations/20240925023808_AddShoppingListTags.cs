07faeec94f786464d3e78eeba1b7b4e54d724b72
sbancevic <bancevicsara@gmail.com>
2024-09-24 21:41:44 -0500

    ShoppingListTag

---
+++ Syncify.Web.Server/Migrations/20240925023808_AddShoppingListTags.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Syncify.Web.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShoppingListTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingListTags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    RecipeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingListTags", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShoppingListTags_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingListTags_RecipeId",
+                table: "ShoppingListTags",
+                column: "RecipeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShoppingListTags");
+        }
+    }
+}
