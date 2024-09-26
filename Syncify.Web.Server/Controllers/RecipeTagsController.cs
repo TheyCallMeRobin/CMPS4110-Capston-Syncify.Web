40610d336d92019ffd7e8ba4a3ba289d5d23c8f9
sbancevic <98131065+sbancevic@users.noreply.github.com>
2024-09-25 23:21:48 -0500

    Create RecipeTagsController.cs

---
+++ Syncify.Web.Server/Controllers/RecipeTagsController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using Syncify.Common;
+
+namespace Syncify.Web.Server.Features.Recipes
+{
+    [ApiController]
+    [Route("api/recipetags")]
+    public class RecipeTagsController : ControllerBase
+    {
+        private readonly RecipeTagService _recipeTagService;
+
+        public RecipeTagsController(RecipeTagService recipeTagService)
+        {
+            _recipeTagService = recipeTagService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Response<List<RecipeTagDto>>>> GetTags()
+        {
+            var result = await _recipeTagService.GetTags();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Response<RecipeTagDto>>> GetTagById(int id)
+        {
+            var result = await _recipeTagService.GetTagById(id);
+            if (result.IsError)
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Response<RecipeTagDto>>> CreateTag([FromBody] RecipeTagCreateDto dto)
+        {
+            var result = await _recipeTagService.CreateTag(dto);
+            return CreatedAtAction(nameof(GetTagById), new { id = result.Data.Id }, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Response<RecipeTagDto>>> UpdateTag(int id, [FromBody] RecipeTagCreateDto dto)
+        {
+            var result = await _recipeTagService.UpdateTag(id, dto);
+            if (result.IsError)
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Response<bool>>> DeleteTag(int id)
+        {
+            var result = await _recipeTagService.DeleteTag(id);
+            if (result.IsError)
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
+        }
+    }
+}
