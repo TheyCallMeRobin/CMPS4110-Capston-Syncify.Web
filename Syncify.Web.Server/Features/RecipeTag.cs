cea2f283e62e99423f6e2be8a960fcabbc0298b4
sbancevic <98131065+sbancevic@users.noreply.github.com>
2024-09-25 23:15:29 -0500

    Update and rename Syncify.Web.Server/Features/Recipes/ShoppingListTag.cs to Syncify.Web.Server/Features/RecipeTag.cs

--- Syncify.Web.Server/Features/RecipeTag.cs
+++ Syncify.Web.Server/Features/RecipeTag.cs
@@ -1,6 +1,6 @@
 namespace Syncify.Web.Server.Features.Recipes
 {
-    public class ShoppingListTag
+    public class RecipeTag
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
