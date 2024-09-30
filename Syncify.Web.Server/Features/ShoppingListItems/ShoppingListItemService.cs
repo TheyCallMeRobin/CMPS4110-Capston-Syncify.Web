21ad473572f8f2682bb11e4d205e715ec4bca27d
Samuel Echols <samuel.echols@selu.edu>
2024-09-30 11:56:52 -0500

    wip: began creating service

---
+++ Syncify.Web.Server/Features/ShoppingListItems/ShoppingListItemService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Syncify.Web.Server.Data;
+
+namespace Syncify.Web.Server.Features.ShoppingListItems;
+
+public interface IShoppingListItemService
+{
+    Task<List<ShoppingListItem>> CreateItemsFromRecipe(int recipeId);
+}
+
+public class ShoppingListItemService : IShoppingListItemService
+{
+
+    private readonly DataContext _dataContext;
+
+    public ShoppingListItemService(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    
+    
+    private Task<bool> IsItemUnique(int shoppingListId, string name)
+    {
+        return _dataContext
+            .Set<ShoppingListItem>()
+            .AnyAsync(x => x.ShoppingListId == shoppingListId && x.Name.Equals(name));
+    }
+
+    public Task<List<ShoppingListItem>> CreateItemsFromRecipe(int recipeId)
+    {
+        throw new NotImplementedException();
+    }
+}
+
