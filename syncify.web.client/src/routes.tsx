9aa96c8f411b99c2f6d7c1994598b44a7c87a28f
Edward Christian <edward.christian@selu.edu>
2024-10-07 18:32:21 -0500

    formatting/removing comments

--- syncify.web.client/src/routes.tsx
+++ syncify.web.client/src/routes.tsx
@@ -6,47 +6,47 @@ import { Reminders } from './Routes/Reminders/reminders.tsx';
 import { RegisterPage } from './Routes/RegisterPage/RegisterPage.tsx';
 import { LoginPage } from './Routes/LoginPage/LoginPage.tsx';
 import ShoppingLists from './Routes/ShoppingLists/shopping-lists.tsx';
-import { App } from './App.tsx'; 
+import { App } from './App.tsx';
 
 export const ROUTES = {
-    LoginPage: {
-        path: '/login',
-        element: <LoginPage />,
-    },
-    Dashboard: {
-        path: '/',
-        element: <MainPage />, 
-    },
-    RegisterPage: {
-        path: '/register',
-        element: <RegisterPage />,
-    },
-    Calendars: {
-        path: '/calendars',
-        element: <Calendars />,
-    },
-    Recipes: {
-        path: '/recipes',
-        element: <Recipes />,
-    },
-    Reminders: {
-        path: '/reminders',
-        element: <Reminders />,
-    },
-    ShoppingLists: {
-        path: '/shopping-lists',
-        element: <ShoppingLists />,
-    },
+  LoginPage: {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  Dashboard: {
+    path: '/',
+    element: <MainPage />,
+  },
+  RegisterPage: {
+    path: '/register',
+    element: <RegisterPage />,
+  },
+  Calendars: {
+    path: '/calendars',
+    element: <Calendars />,
+  },
+  Recipes: {
+    path: '/recipes',
+    element: <Recipes />,
+  },
+  Reminders: {
+    path: '/reminders',
+    element: <Reminders />,
+  },
+  ShoppingLists: {
+    path: '/shopping-lists',
+    element: <ShoppingLists />,
+  },
 };
 
 const routes: RouteObject[] = [
-    {
-        element: <App />, 
-        children: Object.values(ROUTES).map(({ path, element }) => ({
-            path,
-            element,
-        })),
-    },
+  {
+    element: <App />,
+    children: Object.values(ROUTES).map(({ path, element }) => ({
+      path,
+      element,
+    })),
+  },
 ];
 
 export const router = createBrowserRouter(routes);
