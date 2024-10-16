882d31ab62791586b4acdf5d4d516a8db1d6b322
Samuel Echols <samuel.echols@selu.edu>
2024-10-16 14:45:18 -0500

    feat: Created controllers

---
+++ Syncify.Web.Server/Controllers/FamilyCalendarController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Syncify.Web.Server.Extensions;
+using Syncify.Web.Server.Features.FamilyCalendars;
+
+namespace Syncify.Web.Server.Controllers;
+
+[ApiController]
+[Route("api/family-calendars")]
+public class FamilyCalendarController : ControllerBase
+{
+    private readonly IFamilyCalendarService _familyCalendarService;
+
+    public FamilyCalendarController(IFamilyCalendarService familyCalendarService)
+    {
+        _familyCalendarService = familyCalendarService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Response<FamilyCalendarGetDto>>> AddCalendarToFamily(
+        [FromBody] FamilyCalendarCreateDto dto)
+    {
+        var data = await _familyCalendarService.AddCalendarToFamily(dto with
+        {
+            CreatedByUserId = HttpContext.User.GetCurrentUserId() ?? 0
+        });
+        return Ok(data);
+    }
+
+    [HttpGet("family/{familyId}")]
+    public async Task<ActionResult<Response<List<FamilyCalendarGetDto>>>> GetFamilyCalendars(int familyId)
+    {
+        var data = await _familyCalendarService.GetFamilyCalendarsByFamilyId(familyId);
+        return Ok(data);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Response<FamilyCalendarGetDto>>> GetFamilyCalendarById(int id)
+    {
+        var data = await _familyCalendarService.GetFamilyCalendarById(id);
+        return Ok(data);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Response>> RemoveCalendarFromFamily(int id)
+    {
+        var data = await _familyCalendarService.RemoveCalendarFromFamily(id);
+        return Ok(data);
+    }
+}
---
+++ Syncify.Web.Server/Controllers/FamilyRecipeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Syncify.Web.Server.Extensions;
+using Syncify.Web.Server.Features.FamilyRecipes;
+
+namespace Syncify.Web.Server.Controllers;
+
+[ApiController]
+[Route("api/family-recipes")]
+public class FamilyRecipeController : ControllerBase
+{
+    private readonly IFamilyRecipeService _familyRecipeService;
+
+    public FamilyRecipeController(IFamilyRecipeService familyRecipeService)
+    {
+        _familyRecipeService = familyRecipeService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Response<FamilyRecipeGetDto>>> CreateFamilyRecipe(
+        [FromBody] FamilyRecipeCreateDto dto)
+    {
+        var data = await _familyRecipeService.CreateFamilyRecipe(dto with
+        {
+            CreatedByUserId = HttpContext.User.GetCurrentUserId() ?? 0
+        });
+        return Ok(data);
+    }
+
+    [HttpGet("family/{familyId}")]
+    public async Task<ActionResult<Response<List<FamilyRecipeGetDto>>>> GetFamilyRecipes(int familyId)
+    {
+        var data = await _familyRecipeService.GetFamilyRecipesByFamilyId(familyId);
+        return Ok(data);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Response<FamilyRecipeGetDto>>> GetFamilyRecipeById(int id)
+    {
+        var data = await _familyRecipeService.GetFamilyRecipeById(id);
+        return Ok(data);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Response>> RemoveRecipeFromFamily(int id)
+    {
+        var data = await _familyRecipeService.RemoveRecipeFromFamily(id);
+        return Ok(data);
+    }
+}
