68b824d0f17d4ca4af4916ee7168434500f94c35
Samuel Echols <samuel.echols@selu.edu>
2024-10-16 19:13:18 -0500

    feat: added default 0 value to prevent a null return

--- Syncify.Web.Server/Extensions/UserPrincipalExtensions.cs
+++ Syncify.Web.Server/Extensions/UserPrincipalExtensions.cs
@@ -4,14 +4,10 @@ namespace Syncify.Web.Server.Extensions;
 
 public static class UserPrincipalExtensions
 {
-    public static int? GetCurrentUserId(this ClaimsPrincipal claimsPrincipal)
+    public static int GetCurrentUserId(this ClaimsPrincipal claimsPrincipal)
     {
         var userIdClaimValue = claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
-        if (userIdClaimValue == null)
-        {
-            return null;
-        }
-        return int.Parse(userIdClaimValue);
+        return int.TryParse(userIdClaimValue, out int id) ? id : 0;
     }
 
     public static string? GetCurrentUserName(this ClaimsPrincipal claimsPrincipal)
