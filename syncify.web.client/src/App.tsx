d1eb7d14c3a9fd945cf82aee8d71a95fb98577d0
Robin White <104403739+TheyCallMeRobin@users.noreply.github.com>
2024-10-23 22:07:52 -0500

    Final Changes for Midterm

--- syncify.web.client/src/App.tsx
+++ syncify.web.client/src/App.tsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './api/generated/config';
 import './App.css';
-import logo from './../public/Syncify.png';
+import logo from './assets/Syncify.png';
 import { useUser } from './auth/auth-context.tsx';
 import { ROUTES } from './routes.tsx';
 import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
@@ -39,18 +39,24 @@ export const App: React.FC = () => {
         <div className="d-flex min-vh-100">
             {!shouldHideNavBar && (
                 <nav className={`sidebar-navbar navbar navbar-expand-lg navbar-dark flex-column vh-100 sidebar ${isSidebarCollapsed ? 'collapsed' : ''}`}>
-                    <div className="d-flex justify-content-center align-items-center w-100">
-                        <button className="btn btn-link nav-link text-light toggle-btn" onClick={toggleSidebar}>
+                    <ul className="navbar-nav flex-column w-100 icon-list">
+                        <button className="btn btn-link nav-item text-light toggle-btn" onClick={toggleSidebar}>
                             {isSidebarCollapsed ? (
                                 <FaAlignJustify size={24} />
-                            ) : (<img src={logo} alt="Syncify" className="sidebar-logo" style={{ maxWidth: '120px', height: 'auto' }} />
+                            ) : (<img src={logo} alt="Syncify" className="sidebar-logo" />
                             )}
                         </button>
-                    </div>
-                    <ul className="navbar-nav flex-column w-100 icon-list">
+                        <li className="nav-item align-text-">
+                            <Link
+                                className="nav-link"
+                                to="/"
+                            >
+                                <FaHome /> {!isSidebarCollapsed && ' Home'}
+                            </Link>
+                        </li>
                         <li className="nav-item">
                             <Link
-                                className="nav-link d-flex align-items-center justify-content-center"
+                                className="nav-link"
                                 to="/calendars"
                             >
                                 <FaCalendarAlt /> {!isSidebarCollapsed && ' Calendar'}
@@ -58,7 +64,7 @@ export const App: React.FC = () => {
                         </li>
                         <li className="nav-item">
                             <Link
-                                className="nav-link d-flex align-items-center justify-content-center"
+                                className="nav-link"
                                 to="/reminders"
                             >
                                 <FaBell /> {!isSidebarCollapsed && ' Reminder'}
@@ -66,7 +72,7 @@ export const App: React.FC = () => {
                         </li>
                         <li className="nav-item">
                             <Link
-                                className="nav-link d-flex align-items-center justify-content-center"
+                                className="nav-link"
                                 to="/recipes"
                             >
                                 <FaBook /> {!isSidebarCollapsed && ' Recipes'}
@@ -74,39 +80,10 @@ export const App: React.FC = () => {
                         </li>
                         <li className="nav-item">
                             <Link
-                                className="nav-link d-flex align-items-center justify-content-center"
+                                className="nav-link"
                                 to="/shopping-lists"
                             >
                                 <FaShoppingCart /> {!isSidebarCollapsed && ' Shopping List'}
-                            </Link>
-                        </li>
-                    </ul>
-
-                    <div className="flex-grow-1"></div>
-
-                    <ul className="navbar-nav flex-column w-100 mb-3 bottom-links icon-list">
-                        <li className="nav-item mb-2">
-                            <Link
-                                className="nav-link d-flex align-items-center justify-content-center"
-                                to="/account-settings"
-                            >
-                                <FaCog /> {!isSidebarCollapsed && ' Account Settings'}
-                            </Link>
-                        </li>
-                        <li className="nav-item mb-2">
-                            <Link
-                                className="nav-link d-flex align-items-center justify-content-center"
-                                to="/register"
-                            >
-                                <FaUserPlus /> {!isSidebarCollapsed && ' Invite'}
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link
-                                className="nav-link d-flex align-items-center justify-content-center"
-                                to="/"
-                            >
-                                <FaHome /> {!isSidebarCollapsed && ' Home'}
                             </Link>
                         </li>
                     </ul>
@@ -117,38 +94,6 @@ export const App: React.FC = () => {
                 {!shouldHideNavBar && (
                     <nav className="header-navbar navbar navbar-expand-lg navbar-dark">
                         <div className="container-fluid justify-content-between">
-                            <ul className="navbar-nav">
-                                <li className="nav-item dropdown">
-                                    <Link
-                                        className="nav-link dropdown-toggle"
-                                        to="/"
-                                        id="familyDropdown"
-                                        role="button"
-                                        data-bs-toggle="dropdown"
-                                        aria-expanded="false"
-                                    >
-                                        Family
-                                    </Link>
-                                    <ul className="dropdown-menu" aria-labelledby="familyDropdown">
-                                        <li>
-                                            <Link className="dropdown-item" to="/">
-                                                Member 1
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link className="dropdown-item" to="/">
-                                                Member 2
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link className="dropdown-item" to="/">
-                                                Member 3
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-
                             <ul className="navbar-nav ms-auto">
                                 <li className="nav-item dropdown">
                                     <Link
--- syncify.web.client/src/MainPage.tsx
+++ syncify.web.client/src/MainPage.tsx
@@ -1,12 +1,9 @@
-
 import React from 'react';
-
-import './MainPage.css';
 import { AuthenticationService } from "./api/generated/AuthenticationService.ts";
 
 export const MainPage: React.FC = () => {
     return (
-        <div>
+        <div className="page-content">
             <div className="text-center">
                 <h2 className="text-center text-highlight mb-4">This Is The Syncify Landing Page!</h2>
             </div>
