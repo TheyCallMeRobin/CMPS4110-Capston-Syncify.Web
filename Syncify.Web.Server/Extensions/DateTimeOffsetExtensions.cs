14b29d8624b4a4aa15b952f5b8cad1c6f08b59ad
Samuel Echols <samuel.echols@selu.edu>
2024-10-29 11:30:32 -0500

    feat: added extensions

---
+++ Syncify.Web.Server/Extensions/DateTimeOffsetExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Syncify.Web.Server.Extensions;
+
+public static class DateTimeOffsetExtensions
+{
+    public static DateTimeOffset EndOfDay(this DateTimeOffset dateTime)
+    {
+        return new DateTimeOffset(dateTime.Year, dateTime.Month, dateTime.Day, 23, 59, 59, 999, dateTime.Offset);
+    }
+}
