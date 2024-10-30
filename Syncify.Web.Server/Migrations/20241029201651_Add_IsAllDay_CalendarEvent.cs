71f2c70cbb17b4dec8c4ae847014f2209fde7029
Samuel Echols <samuel.echols@selu.edu>
2024-10-30 10:36:38 -0500

    feat: generated migrations

---
+++ Syncify.Web.Server/Migrations/20241029201651_Add_IsAllDay_CalendarEvent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Syncify.Web.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_IsAllDay_CalendarEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAllDay",
+                table: "CalendarEvents",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAllDay",
+                table: "CalendarEvents");
+        }
+    }
+}
