8d22eb5e50ff5d5e5e28dd6296f41f36dedcb27e
Samuel Echols <samuel.echols@selu.edu>
2024-11-13 13:17:43 -0500

    feat: added loading state to the auth-context to assist in component state handling

--- syncify.web.client/src/auth/auth-context.tsx
+++ syncify.web.client/src/auth/auth-context.tsx
@@ -1,58 +1,74 @@
-﻿import { createContext, FC, ReactNode, useContext, useState } from 'react';
+﻿import {
+  createContext,
+  FC,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import { UserGetDto } from '../api/generated/index.defs.ts';
 import { useAsync } from 'react-use';
 import { AuthenticationService } from '../api/generated/AuthenticationService.ts';
 
 type AuthContextState = {
-    user?: UserGetDto;
-    clearUser: () => void;
+  user?: UserGetDto;
+  clearUser: () => void;
+  loading?: boolean;
 };
 
 const initialState: AuthContextState = {
-    user: undefined,
-    clearUser: () => { },
+  user: undefined,
+  clearUser: () => {},
+  loading: true,
 };
 
-const AuthContext = createContext<AuthContextState>(initialState);
+export const AuthContext = createContext<AuthContextState>(initialState);
 
 export const useUser = (): UserGetDto | undefined => {
-    const authContext = useContext(AuthContext);
+  const authContext = useContext(AuthContext);
 
-    if (!authContext)
-        throw new Error("`useUser' hook must be used within an AuthContext.");
+  if (!authContext)
+    throw new Error("`useUser' hook must be used within an AuthContext.");
 
-    return authContext.user;
+  return authContext.user;
 };
 
 export const AuthProvider: FC<{ children?: ReactNode }> = ({ children }) => {
-    const [authState, setAuthState] = useState<AuthContextState>(initialState);
+  const [authState, setAuthState] = useState<AuthContextState>(initialState);
 
-    const fetchUser = useAsync(async () => {
-        const response = await AuthenticationService.me();
-        if (response.hasErrors || !response.data) {
-            return initialState;
-        }
-        const state: AuthContextState = {
-            user: response.data,
-            clearUser: () => setAuthState(initialState),
-        };
-        setAuthState(state);
-        return state;
-    }, []);
+  const fetchUser = useAsync(async () => {
+    const response = await AuthenticationService.me();
+    if (response.hasErrors || !response.data) {
+      return initialState;
+    }
+    const state: AuthContextState = {
+      user: response.data,
+      clearUser: () => setAuthState(initialState),
+      loading: false,
+    };
+    setAuthState(state);
+    return state;
+  }, []);
 
-    return (
-        <>
-            {fetchUser.loading ? (
-                <div className="d-flex justify-content-center align-content-center text-center">
-                    <div className="spinner-border" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                    </div>
-                </div>
-            ) : (
-                <AuthContext.Provider value={authState}>
-                    {children}
-                </AuthContext.Provider>
-            )}
-        </>
-    );
+  useEffect(() => {
+    if (!fetchUser.loading) {
+      setAuthState(fetchUser.value || initialState);
+    }
+  }, [fetchUser.loading, fetchUser.value]);
+
+  return (
+    <>
+      {authState.loading ? (
+        <div className="d-flex justify-content-center align-content-center text-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <AuthContext.Provider value={authState}>
+          {children}
+        </AuthContext.Provider>
+      )}
+    </>
+  );
 };
