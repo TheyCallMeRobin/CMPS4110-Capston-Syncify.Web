02da8ea3acfda9e56ce91bb16d4ed0eb525e61aa
Samuel Echols <samuel.echols@selu.edu>
2024-11-14 19:31:50 -0500

    feat: fixed authentication refresh

--- syncify.web.client/src/auth/auth-context.tsx
+++ syncify.web.client/src/auth/auth-context.tsx
@@ -1,25 +1,19 @@
-﻿import {
-  createContext,
-  FC,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
+﻿import { createContext, FC, ReactNode, useContext, useState } from 'react';
 import { UserGetDto } from '../api/generated/index.defs.ts';
-import { useAsync } from 'react-use';
+import { useAsyncFn } from 'react-use';
 import { AuthenticationService } from '../api/generated/AuthenticationService.ts';
+import { useSubscription } from '../hooks/use-subscription.ts';
 
 type AuthContextState = {
-  user?: UserGetDto;
-  clearUser: () => void;
+  user?: UserGetDto | undefined;
+  clearUser: () => void | undefined;
   loading?: boolean;
 };
 
 const initialState: AuthContextState = {
   user: undefined,
-  clearUser: () => {},
-  loading: true,
+  clearUser: () => undefined,
+  loading: false,
 };
 
 export const AuthContext = createContext<AuthContextState>(initialState);
@@ -36,29 +30,26 @@ export const useUser = (): UserGetDto | undefined => {
 export const AuthProvider: FC<{ children?: ReactNode }> = ({ children }) => {
   const [authState, setAuthState] = useState<AuthContextState>(initialState);
 
-  const fetchUser = useAsync(async () => {
+  const [fetchState, fetchUser] = useAsyncFn(async () => {
     const response = await AuthenticationService.me();
-    if (response.hasErrors || !response.data) {
+    if (response.hasErrors) {
       return initialState;
     }
+
     const state: AuthContextState = {
-      user: response.data,
+      user: response.data!,
       clearUser: () => setAuthState(initialState),
       loading: false,
     };
-    setAuthState(state);
-    return state;
+
+    setAuthState(() => state);
   }, []);
 
-  useEffect(() => {
-    if (!fetchUser.loading) {
-      setAuthState(fetchUser.value || initialState);
-    }
-  }, [fetchUser.loading, fetchUser.value]);
+  useSubscription('auth-trigger', fetchUser);
 
   return (
     <>
-      {authState.loading ? (
+      {fetchState.loading || authState.loading ? (
         <div className="d-flex justify-content-center align-content-center text-center">
           <div className="spinner-border" role="status">
             <span className="visually-hidden">Loading...</span>
