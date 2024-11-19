55004602d9ccae63df675582428cb4f7d50b18d8
sbancevic <98131065+sbancevic@users.noreply.github.com>
2024-11-18 20:30:20 -0600

    Updated Unit property handling. The UnitsDropdown now fetches units from the backend, no more hard coding in every file (#83)

---
+++ Syncify.Web.Server/Migrations/20241118031954_UpdateRecipeIngredientUnitToEnum.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Syncify.Web.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRecipeIngredientUnitToEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Unit",
+                table: "ShoppingListItems",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "Count",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(6)",
+                oldMaxLength: 6,
+                oldDefaultValue: "count");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Unit",
+                table: "RecipeIngredients",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Unit",
+                table: "ShoppingListItems",
+                type: "nvarchar(6)",
+                maxLength: 6,
+                nullable: false,
+                defaultValue: "count",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10,
+                oldDefaultValue: "Count");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Unit",
+                table: "RecipeIngredients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
