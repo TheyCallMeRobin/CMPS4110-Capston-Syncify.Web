5f41568f4b136967c13a6ffcb00fc7d0fbb7b33a
Zachary Duncan <71148283+Zach-Duncan@users.noreply.github.com>
2024-11-19 16:07:40 -0600

    Added an invite menu to the App.tsx file. this allows a user to accept or decline and invite sent to them.

--- syncify.web.client/src/App.tsx
+++ syncify.web.client/src/App.tsx
@@ -15,12 +15,22 @@ import {
   FaShoppingCart,
   FaUser,
   FaUsers,
+  FaEnvelope,
+  FaTimes,
 } from 'react-icons/fa';
 import { useAsyncFn } from 'react-use';
 import { AuthenticationService } from './api/generated/AuthenticationService.ts';
+import { FamilyInviteService } from './api/generated/FamilyInviteService.ts';
+import { FamilyInviteGetDto, InviteStatus, ChangeInviteStatusDto } from './api/generated/index.defs';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App: React.FC = () => {
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+  const [invites, setInvites] = useState<FamilyInviteGetDto[]>([]);
+  const [loadingInvites, setLoadingInvites] = useState<boolean>(false);
+  const [loadingAction, setLoadingAction] = useState<number | null>(null);
+  const [isInviteMenuOpen, setIsInviteMenuOpen] = useState<boolean>(false);
   const user = useUser();
   const navigate = useNavigate();
   const location = useLocation();
@@ -44,11 +54,45 @@ export const App: React.FC = () => {
   const noNavBarPages = [ROUTES.LoginPage.path, ROUTES.RegisterPage.path];
   const shouldHideNavBar = noNavBarPages.includes(location.pathname);
 
+  const fetchInvites = async () => {
+    setLoadingInvites(true);
+    if (user?.id) {
+      const inviteResponse = await FamilyInviteService.getInvitesByUserId({ userId: user.id });
+
+      const invitesData = Array.isArray(inviteResponse.data)
+          ? inviteResponse.data.filter((invite): invite is FamilyInviteGetDto => invite !== null)
+          : [inviteResponse.data].filter((invite): invite is FamilyInviteGetDto => invite !== null);
+
+      setInvites(invitesData);
+    }
+    setLoadingInvites(false);
+  };
+
+  useEffect(() => {
+    fetchInvites();
+  }, [user]);
+
+  const handleInboxClick = () => {
+    setIsInviteMenuOpen(!isInviteMenuOpen);
+  };
+
+  const handleInviteAction = async (inviteId: number, action: 'accept' | 'decline') => {
+    setLoadingAction(inviteId);
+    const status = action === 'accept' ? InviteStatus.Accepted : InviteStatus.Declined;
+    const changeInviteStatusDto: ChangeInviteStatusDto = { id: inviteId, status };
+
+    await FamilyInviteService.changeInviteStatus({ body: changeInviteStatusDto });
+    setInvites(prevInvites => prevInvites.filter(invite => invite.id !== inviteId));
+
+    toast.success(`Invite successfully ${action === 'accept' ? 'accepted' : 'declined'}`);
+    setLoadingAction(null);
+  };
+
   return (
       <div className="d-flex min-vh-100">
         {!shouldHideNavBar && (
             <nav
-                className={`sidebar-navbar navbar navbar-expand-lg navbar-dark flex-column vh-100 sidebar position-fixed overflow-y-auto  ${
+                className={`sidebar-navbar navbar navbar-expand-lg navbar-dark flex-column vh-100 position-fixed overflow-y-auto ${
                     isSidebarCollapsed ? 'collapsed' : ''
                 }`}
                 style={{ zIndex: '999' }}
@@ -64,7 +108,7 @@ export const App: React.FC = () => {
                       <img src={logo} alt="Syncify" className="sidebar-logo" />
                   )}
                 </button>
-                <li className="nav-item align-text-">
+                <li className="nav-item align-text-center">
                   <Link className="nav-link" to="/">
                     <FaHome /> {!isSidebarCollapsed && ' Home'}
                   </Link>
@@ -100,9 +144,21 @@ export const App: React.FC = () => {
 
         <div className="flex-grow-1 d-flex flex-column">
           {!shouldHideNavBar && (
-              <nav className="header-navbar navbar navbar-expand-lg navbar-dark">
+              <nav className="header-navbar navbar navbar-expand-lg navbar-dark bg-success border-bottom border-dark">
                 <div className="container-fluid justify-content-between">
-                  <ul className="navbar-nav ms-auto">
+                  <ul className="navbar-nav ms-auto d-flex align-items-center icons-container gap-2">
+                    <li className="nav-item position-relative">
+                      <button
+                          className="nav-link btn text-light"
+                          onClick={handleInboxClick}
+                          style={{ position: 'relative' }}
+                      >
+                        <FaEnvelope size={24} />
+                        {invites.length > 0 && (
+                            <span className="notification-dot"></span>
+                        )}
+                      </button>
+                    </li>
                     <li className="nav-item dropdown">
                       <Link
                           className="nav-link dropdown-toggle"
@@ -147,11 +203,55 @@ export const App: React.FC = () => {
           )}
           <main
               className={'px-5'}
-              style={{ marginLeft: isSidebarCollapsed ? '80px' : '180px' }}
+              style={{ marginLeft: isSidebarCollapsed ? '50px' : '180px' }}
           >
+            {isInviteMenuOpen && invites.length > 0 && (
+                <div className="invites-dropdown position-absolute bg-white border border-secondary rounded-2 p-2" style={{ right: '0', top: '50px', zIndex: '100', width: '300px' }}>
+                  <div className="d-flex justify-content-between align-items-center mb-2">
+                    <strong>Invitations</strong>
+                    <button className="btn btn-link text-decoration-none p-0" onClick={() => setIsInviteMenuOpen(false)}>
+                      <FaTimes />
+                    </button>
+                  </div>
+                  <ul className="list-group">
+                    {invites.map((invite) => (
+                        <li key={invite.id} className="list-group-item">
+                          <div>
+                            <strong>{invite.sentByUserFullName}</strong> invited you to join the family: {invite.familyName}
+                          </div>
+                          <div>Status: {invite.status}</div>
+                          {invite.expiresOn && (
+                              <div>Expires on: {new Date(invite.expiresOn).toLocaleDateString()}</div>
+                          )}
+                          <div className="d-flex justify-content-between mt-3">
+                            <button
+                                className="btn btn-success btn-sm"
+                                onClick={() => handleInviteAction(invite.id, 'accept')}
+                                disabled={loadingAction === invite.id}
+                            >
+                              {loadingAction === invite.id ? 'Accepting...' : 'Accept'}
+                            </button>
+                            <button
+                                className="btn btn-danger btn-sm"
+                                onClick={() => handleInviteAction(invite.id, 'decline')}
+                                disabled={loadingAction === invite.id}
+                            >
+                              {loadingAction === invite.id ? 'Declining...' : 'Decline'}
+                            </button>
+                          </div>
+                        </li>
+                    ))}
+                  </ul>
+                </div>
+            )}
+
+            {loadingInvites && <p>Loading invites...</p>}
+
             <Outlet />
           </main>
         </div>
+
+        <ToastContainer />
       </div>
   );
 };
