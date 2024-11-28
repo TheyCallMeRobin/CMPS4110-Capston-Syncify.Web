f91cedb7bb08f445a3dbf15400fb7270fa5991d7
Samuel Echols <samuel.echols@selu.edu>
2024-11-28 13:33:50 -0600

    feat: Added an "offline" state to the weather widget. This idea came about because I was coding in the car

--- syncify.web.client/src/main-page/MainPage.tsx
+++ syncify.web.client/src/main-page/MainPage.tsx
@@ -4,43 +4,43 @@ import { TodaysTodos } from './todays-todos.tsx';
 import { RecipeOfTheDay } from './recipe-of-the-day.tsx';
 import { FamilyMembers } from './family-members.tsx';
 import { QuickActions } from './quick-actions.tsx';
-import { WeatherWidget } from './weather-widget.tsx';
+import { WeatherWidget } from './weather/weather-widget.tsx';
 
 export const MainPage: React.FC = () => {
-    return (
-        <>
-            <div className="container">
-                <div className="row">
-                    <div className={'col-12'}>
-                        <h2 className={'text-center text-highlight'}>Dashboard</h2>
-                    </div>
-                </div>
-            </div>
-            <div className={'row'}>
-                <div className={'col-lg-4 col-md-6'}>
-                    <WeatherWidget />
-                </div>
-                <div className={'col-lg-4 col-md-6'}>
-                    <UpcomingEventsCard />
-                </div>
-                <div className={'col-lg-4 col-md-6'}>
-                    <TodaysTodos />
-                </div>
-                <div className={'col-lg-4 col-md-6'}>
-                    <RecipeOfTheDay />
-                </div>
-                <div className={'col-lg-4 col-md-6'}>
-                    <FamilyMembers />
-                </div>
-                <div className={'col-lg-4 col-md-6'}>
-                    <QuickActions />
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className={'col-12'}>
+            <h2 className={'text-center text-highlight'}>Dashboard</h2>
+          </div>
+        </div>
+      </div>
+      <div className={'row'}>
+        <div className={'col-lg-4 col-md-6'}>
+          <WeatherWidget />
+        </div>
+        <div className={'col-lg-4 col-md-6'}>
+          <UpcomingEventsCard />
+        </div>
+        <div className={'col-lg-4 col-md-6'}>
+          <TodaysTodos />
+        </div>
+        <div className={'col-lg-4 col-md-6'}>
+          <RecipeOfTheDay />
+        </div>
+        <div className={'col-lg-4 col-md-6'}>
+          <FamilyMembers />
+        </div>
+        <div className={'col-lg-4 col-md-6'}>
+          <QuickActions />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export const cardStyle = {
-    width: '100%',
-    height: 'auto',
+  width: '100%',
+  height: 'auto',
 };
