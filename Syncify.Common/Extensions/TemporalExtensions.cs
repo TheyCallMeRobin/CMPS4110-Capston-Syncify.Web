d63e212979627e0675093116a8232b379505c907
Samuel Echols <samuel.echols@selu.edu>
2024-11-28 17:28:35 -0600

    feat: created temporal extensions

---
+++ Syncify.Common/Extensions/TemporalExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Syncify.Common.Extensions;
+
+public static class DateTimeOffsetExtensions
+{
+    public static TimeOnly ToTimeOnly(this DateTimeOffset dt)
+    {
+        return TimeOnly.FromTimeSpan(dt.TimeOfDay).AddMinutes(dt.TotalOffsetMinutes);
+    }
+}
